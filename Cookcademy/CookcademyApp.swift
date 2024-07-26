8fec68f009bef95094d5c9cf838f6945a134e18b
ArmaniCod3 <amani_g_addison@hotmail.com>
2024-07-26 15:28:50 +0100

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Amani White on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
